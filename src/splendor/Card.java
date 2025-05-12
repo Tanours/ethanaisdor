6766ebd814d2593c7f17cff7f186f75896080685
Ethan Dorothee <tanours@MacBook-Air-de-Ethan-2.local>
2025-05-12 08:32:30 +0200

    constructor Card

--- src/splendor/Card.java
+++ src/splendor/Card.java
@@ -2,9 +2,15 @@ package splendor;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public record Card(int id,Stones stone ,HashMap<Stones,Integer> needStones,int prestige) {
 	public Card{
+		if(id<0||prestige<0) {
+			throw new IllegalArgumentException();
+		}
+		Objects.requireNonNull(stone);
+		Objects.requireNonNull(needStones);
 		
 	}
 	
